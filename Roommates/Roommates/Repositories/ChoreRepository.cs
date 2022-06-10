5bde176c9e4f19d0dbc381254219d2fea4418b4f
Sippy Chanthaphaychith <sipra.chant@gmail.com>
2022-06-09 22:31:30 -0500

    created Repositories

---
+++ Roommates/Roommates/Repositories/ChoreRepository.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    public class ChoreRepository : BaseRepository
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+
+        {
+            //  We must "use" the database connection.
+            //  Because a database is a shared resource (other applications may be using it too) we must
+            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
+            //  interact with the database and we Close() them when we're finished.
+            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
+            //  For database connections, this means the connection will be properly closed.
+            using (SqlConnection conn = Connection)
+            {
+                // Note, we must Open() the connection, the "using" block doesn't do that for us.
+                conn.Open();
+
+                // We must "use" commands too.
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // Here we setup the command with the SQL we want to execute before we execute it.
+                    cmd.CommandText = "SELECT Id, Name FROM Room Chore";
+
+                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // A list to hold the rooms we retrieve from the database.
+                        List<Chore> chores = new List<Chore>();
+
+                        // Read() will return true if there's more data to read. reader.Read()) will advance to the next row. 
+                        
+                        while (reader.Read())
+                        {
+                            // The "ordinal" is the numeric position of the column in the query results.
+                            //  For our query, "Id" has an ordinal value of 0 and "Name" is 1.
+
+                            int idColumnPosition = reader.GetOrdinal("Id");
+
+                            // We user the reader's GetXXX methods to get the value for a particular ordinal.
+                            int idValue = reader.GetInt32(idColumnPosition);
+
+                            int nameColumnPosition = reader.GetOrdinal("Name");
+                            string nameValue = reader.GetString(nameColumnPosition);
+
+
+
+                            // Now let's create a new room object using the data from the database.
+                            Chore chore = new Chore
+                            {
+                                Id = idValue,
+                                Name = nameValue,
+
+                            };
+
+                            // ...and add that room object to our list.
+                            chores.Add(chore);
+                        }
+                        // Return the list of rooms who whomever called this method.
+                        return chores;
+                    }
+                    }
+                }
+                }
+        public Chore GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+
+                        // If we only expect a single row back from the database, we don't need a while loop.
+                        if (reader.Read())
+                        {
+                            chore = new Chore
+                            {
+                                Id = id,
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                
+                            };
+                        }
+                        return chore;
+                    }
+
+                }
+            }
+        }
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name) 
+                                         OUTPUT INSERTED.Id 
+                                         VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+
+            // when this method is finished we can look in the database and see the new room.
+        }
+    }
+     }
+    
+
+
